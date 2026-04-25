481815d0c87f6032a96c6d827db0baa9bf53d707
jeremi-24 <jeremiekoue8@gmail.com>
2026-04-25 21:20:40 +0000

    webhoj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                     <h1 class="text-4xl font-bold tracking-tight text-white mb-2">
                         System <span class="text-blue-500">Dashboard</span>
                     </h1>
-                    <p class="text-blue-400 font-bold animate-pulse">Auto-Deploy via Webhook : TEST RÉUSSI ! 🚀</p>
+                    <p class="text-blue-400 font-bold animate-pulse">Clean App : TEST RÉUSSI ! 🚀</p>
                 </div>
                 <div class="text-right">
                     <span class="px-3 py-1 bg-green-500/10 text-green-400 rounded-full text-xs font-bold uppercase tracking-wider border border-green-500/20">
