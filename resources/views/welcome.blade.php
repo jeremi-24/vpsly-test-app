07d9e1b3e7eea0379aa9614fabbf466c5299ffe0
jeremi-24 <jeremiekoue8@gmail.com>
2026-04-26 02:57:29 +0000

    pour la science

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
                 <h1 class="text-4xl font-bold tracking-tight text-white mb-2">
                     System <span class="text-blue-500">Dashboard</span>
                 </h1>
-                <p class="text-blue-400 font-bold animate-pulse">etickonam</p>
+                <p class="text-blue-400 font-bold animate-pulse">Bonjour lomé est dure</p>
             </div>
             <div class="text-right">
                 <span
